0773355f236ffeb4b84ca9ca530e83ededb91828
f83y55 <f83y55@gmail.com>
2022-08-06 15:36:35 +0200

    commit

--- mail_to_website.py
+++ mail_to_website.py
@@ -1,58 +1,101 @@
+from imap_tools import MailBox, AND
+# $ pip install imap-tools
 import os
-import re
+import json
+import pprint
+
+json_pswd_filename = "pswd.json"
+ 
+def input_account_info() -> dict:
+    """Get email account information"""
+    data = {}
+    data["server"] = input("Enter webmail imap server address :\n")
+    data["port"] = input("Enter email imap port :\n")
+    if not data["port"] :
+        data["port"] = 993
+    else :
+        try :
+            data["port"] = int(data["port"])
+        except :
+            data["port"] = 993
+    data["user"] = input("Enter username :\n")
+    data["password"] = input("Enter password :\n")
+    return data
+
+
+def json_load(filename:str=json_pswd_filename) -> dict:
+    """Read a json file containing email account information"""
+    try :
+        with open(filename, 'r', encoding='utf-8') as file:
+            print(f"loading {filename}")
+            data = json.load(file)
+            if isinstance(data, dict) and "server" in data.keys() and "port" in data.keys() and "user" in data.keys() and "password" in data.keys() :
+                return data
+            else :
+                raise NameError("Incorrect json")
+    except Exception as e :
+        print(f"Exception : {e} \n Trying to create a new json file {filename}")
+        data = input_account_info()
+        json_save(data, filename)
+        return data
+
+
+def json_save(data:dict, filename:str=json_pswd_filename) :
+    """Save a json file containing email account information"""
+    try :
+        with open(filename, 'w', encoding='utf-8') as file:
+            json.dump(data, file, indent=4)
+            print(f"saved as {filename}")
+    except Exception as e :
+        print(f"Exception : {e}")
+
+def mailbox_connect(account_data:dict={}, filename:str=json_pswd_filename):
+    """Imap connexion"""
+    text = ""
+    if not account_data:
+        account_data = json_load(filename)
+    # Login :
+    mb = MailBox(account_data["server"]).login(account_data["user"], account_data["password"])
+    messages = mb.fetch()
+    files = []
+    for msg in messages:
+        # Print form and subject
+        print(msg.from_, ': ', msg.subject)
+        # Print the plain text (if there is one)
+        text += msg.text
+        # Add attachments
+        files += [att.payload for att in msg.attachments if att.filename.endswith('.pdf')]
+    return text
 
 def empty_line(st:str):
     """Show if a line only contains tabs and spaces, ie is blank"""
     spaces, tabs = st.count(" "), st.count("\t")
-    return st[-1] == "\n" and len(st) == spaces + tabs + 1
+    return len(st) == spaces + tabs
 
-def text_to_data(filename:str):
-    """Text file to [ [..., ...]:lines ]:parag conversion"""
-    data = []
-    with open(filename) as file:
-        lines = file.readlines()
-        parag = []
-        for line in lines :
-            if empty_line(line) :
-                if parag :
-                    data.append(parag)
-                parag = []
-            else :
-                parag.append(line.strip())
-        data.append(parag)
-    return data
 
-def data_to_html(data:list):
+def text_to_html(text:str):
+    """Plain text file to html"""
     spaces = 4
     html = ""
-    for parag in data :
-        html += f"<p>\n{spaces*' '}" + f"<br>\n{spaces*' '}".join(parag) + "\n</p>\n\n"
-    return html
-
-def text_to_html(filename:str):
-    """Text file to html"""
-    spaces = 4
-    html = ""
-    with open(filename) as file:
-        lines = file.readlines()
-        parag = []
-        for line in lines :
-            if empty_line(line) :
-                if parag :
-                    html += f"<p>\n{spaces*' '}" + f" <br>\n{spaces*' '}".join(parag) + "\n</p>\n\n"
+    lines = text.split('\n')
+    parag = []
+    for line in lines :
+        print(line)
+        if empty_line(line) :
+            if parag :
+                html += f"<p>\n{spaces*' '}" + f" <br>\n{spaces*' '}".join(parag) + "\n</p>\n\n"
                 parag = []
-            else :
-                parag.append(line.strip())
-        html += f"<p>\n{spaces*' '}" + f" <br>\n{spaces*' '}".join(parag) + "\n</p>\n\n"
+        else :
+            parag.append(line)
+    html += f"<p>\n{spaces*' '}" + f" <br>\n{spaces*' '}".join(parag) + "\n</p>\n\n"
     return html
 
-def html_to_file(filename:str, content:str):
+
+def html_to_file(content:str, filename:str="html_out.html"):
     """html to text file"""
     with open(filename+".html", "w") as file:
         file.write(content)
 
 
 if __name__ == "__main__":
-    filename = "test.txt"
-    html_to_file(filename, text_to_html(filename))
-
+    html_to_file(text_to_html(mailbox_connect()))
